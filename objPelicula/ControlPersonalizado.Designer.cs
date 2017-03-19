840bac713fbdda4d8168042c4af70376782db658
Malaherba <lucho_malaherba@hotmail.com>
2017-03-19 21:15:08 +0100

    Subida de proxecto
    Im so sad

---
+++ objPelicula/ControlPersonalizado.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace objPelicula
+{
+    partial class ControlPersonalizado
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_ImageMoviePreview = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_ImageMoviePreview
+            // 
+            this.btn_ImageMoviePreview.BackgroundImage = global::objPelicula.Properties.Resources._default;
+            this.btn_ImageMoviePreview.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_ImageMoviePreview.Location = new System.Drawing.Point(10, 10);
+            this.btn_ImageMoviePreview.Name = "btn_ImageMoviePreview";
+            this.btn_ImageMoviePreview.Size = new System.Drawing.Size(150, 220);
+            this.btn_ImageMoviePreview.TabIndex = 0;
+            this.btn_ImageMoviePreview.UseVisualStyleBackColor = true;
+            // 
+            // ControlPersonalizado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.Controls.Add(this.btn_ImageMoviePreview);
+            this.MinimumSize = new System.Drawing.Size(170, 240);
+            this.Name = "ControlPersonalizado";
+            this.Size = new System.Drawing.Size(170, 240);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_ImageMoviePreview;
+    }
+}
---
+++ objPelicula/ControlPersonalizado.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace objPelicula
+{
+    public partial class ControlPersonalizado : System.Windows.Forms.UserControl
+    {
+        public int codigo;
+        public ControlPersonalizado()
+        {
+            InitializeComponent();
+        }
+
+        public void setImgMovie(string ruta) {
+            //Método que nos permite definir la imagen que vamos a mostrar en el CP.
+            btn_ImageMoviePreview.BackgroundImage = Image.FromFile(ruta);
+        }
+
+        public void setCodigo(int Codigo) {
+            //Método que nos permite definir el codigo de la Pelicula que va a mostrar dicho CP.
+            codigo = Codigo;
+        }
+
+        public Button getButton()
+        {
+            //Método que nos permite retornar el Button contenedor de la imagen.
+            return btn_ImageMoviePreview;
+
+        }
+    }
+}
